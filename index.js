e1589679ac22038876f1d1d8d8934a90c4d9f0de
karen chehade <karen_chehade@hotmail.com>
2021-05-08 23:23:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,24 +14,24 @@ function createEmployeeRecords(arr) {
   return arr.map(s => createEmployeeRecord(s));
 }
 
-function createTimeInEvent(employee , date) {
+function createTimeInEvent(date) {
   let dateTime = date.split(' ')
-  employee.timeInEvents.push({type: "TimeIn",
+  this.timeInEvents.push({type: "TimeIn",
     hour: parseInt(dateTime[1]),
 		date: dateTime[0]})
-return employee
+return this
 }
-function createTimeOutEvent(employee , date) {
+function createTimeOutEvent(date) {
   let dateTime = date.split(' ')
-  employee.timeOutEvents.push({type: "TimeOut",
+  this.timeOutEvents.push({type: "TimeOut",
     hour: parseInt(dateTime[1]),
 		date: dateTime[0]})
-return employee
+return this
 }
 
-function hoursWorkedOnDate(employee , date) {
-  let timeIn = employee.timeInEvents.find(e =>e.date == date)
-	let timeOut = employee.timeOutEvents.find(e =>e.date == date)
+function hoursWorkedOnDate(date) {
+  let timeIn = this.timeInEvents.find(e =>e.date == date)
+	let timeOut = this.timeOutEvents.find(e =>e.date == date)
 	return (timeOut.hour - timeIn.hour) / 100
 }
 
